e4b59b8744343a2f0746100c9a93c1de03a2cc77
Eri Sunarko <erisunarko@travikr.com>
2021-10-10 15:22:47 +0700

    Commit for prod...

---
+++ js/703.2d86d19c.js
@@ -0,0 +1,1 @@
+"use strict";(self["webpackChunkquasar"]=self["webpackChunkquasar"]||[]).push([[703],{5703:(s,t,n)=>{n.r(t),n.d(t,{default:()=>c});var a=n(3673);function e(s,t,n,e,u,r){const c=(0,a.up)("post-list");return(0,a.wg)(),(0,a.j4)(c)}var u=n(5990);const r=(0,a.aZ)({name:"Admin",components:{PostList:u["default"]}});r.render=e;const c=r}}]);
